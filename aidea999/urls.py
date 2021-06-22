5125b7369ab9abc77b7fd4d25006e37221f6e5f0
touchavin <83328543+touchavin@users.noreply.github.com>
2021-06-22 12:34:20 +0700

    Add files via upload

---
+++ aidea999/urls.py
@@ -0,0 +1,44 @@
+"""wedaidea URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from wedaidea.views import register
+from wedaidea.views import upload
+from wedaidea.views import bad
+from wedaidea.views import good
+from wedaidea.views import login
+from wedaidea.views import system
+from wedaidea.views import user
+from wedaidea.views import download
+from wedaidea.views import uploadPage2
+
+from django.conf import settings
+from django.conf.urls.static import static 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', register),
+    path('upload/', upload),
+    path('bad/', bad),
+    path('good/', good),
+    path('login/', login),
+    path('system/', system),
+    path('user/', user),
+    path('download/', download),
+    path('uploadPage2/', uploadPage2),
+    
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
